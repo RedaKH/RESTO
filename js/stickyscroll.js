426019535c7199f68ccd8d7d1dd502fb4db63767
RedaKH <reda.khaldi@gmail.com>
2021-03-27 14:31:42 +0100

    first commit

---
+++ js/stickyscroll.js
@@ -0,0 +1,20 @@
+//detection scroll
+
+window.onscroll = function name(params) {
+    sticky()
+    
+};
+//recuperer les elements
+
+let header = document.getElementById("sticky-header");
+let stickycomponent = header.offsetTop;
+
+//faire la fonction
+function sticky () {
+
+    if (window.pageYOffset>stickycomponent) {
+        header.classList.add("sticky");
+    }else{
+        header.classList.remove("sticky");
+    }
+}
